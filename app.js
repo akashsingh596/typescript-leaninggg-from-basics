a440893f0107f00cf49956758c80764313f60b28
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2025-02-11 13:58:54 +0530

    commit

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+let a = 12;
+let b = 15;
+function abc() {
+    console.log(b);
+}
+abc();
+console.log(a);
---
+++ app.ts
@@ -0,0 +1,9 @@
+let a = 12 ;
+let b = 15 ;
+
+function abc(){
+    console.log(b)
+}
+abc()
+
+console.log(a)
