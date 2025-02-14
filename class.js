52ecb1e8465c87179ba3e7486f7b016fc923c8ec
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2025-02-14 11:13:34 +0530

    class and constructors

---
+++ class.js
@@ -0,0 +1,18 @@
+"use strict";
+class device {
+    constructor() {
+        this.name = "lg";
+        this.price = 1200;
+        this.category = "digital";
+    }
+}
+let d1 = new device();
+let d2 = new device();
+class bottle {
+    constructor() {
+        this.radius = 120;
+        this.price = 20;
+        this.color = "white";
+    }
+}
+let b1 = new bottle();
---
+++ class.ts
@@ -0,0 +1,15 @@
+class device {
+  name = "lg";
+  price = 1200;
+  category = "digital";
+}
+
+let d1 = new device();
+let d2 = new device();
+
+class bottle{
+    radius = 120;
+    price = 20;
+    color ="white"
+}
+let b1  = new bottle()
